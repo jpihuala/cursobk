6990388dfac8bf6935f6b9dc926330958f462c48
Josue <josue@MacBook-Air-de-Josue.local>
2024-07-11 19:46:18 -0300

    clase 9

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
+
 const app = express()
 const PORT = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+app.get('/', (req,res)=>{
+    let testUser={
+        name: "Fabio",
+        last_name: "Bianchi"
+    }
+    res.render('index', testUser)
+})
+
 
 
 app.listen(PORT, () => {
